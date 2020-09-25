69a17b6b1472daed2866c44b53f125e9912f4770
AdamSpannbauer <spannbaueradam@gmail.com>
2020-09-25 07:24:40 -0400

    edit sketch to use dist metric class

--- distance_metric.js
+++ distance_metric.js
@@ -2,15 +2,16 @@
  * Base class for distance metrics/drawings.
  */
 
-class Distance {
+class DistanceMetric {
   /*eslint-disable */
   // eslint was making it be really vertically spaced out
   // i didnt like it
-  name = 'Distance Base Class';
+  name = 'Distance Metric Base Class';
   
   #p1 = createVector();
   #p2 = createVector();
   #p3 = createVector();
+  use_p3 = false;
 
   d12 = 0;
   d13 = 0;
@@ -27,6 +28,7 @@ class Distance {
 
     // Ignore missing p3
     if (p3) {
+      this.use_p3 = true;
       this.#p3.set(p1.x, p1.y);
 
       this.d13 = this.distance(this.#p1, this.#p3);
@@ -36,15 +38,32 @@ class Distance {
 
   distance() {
     // TODO: implement distance calc here
-    return random();
+    return dist(this.#p1.x, this.#p1.y, this.#p2.x, this.#p2.y);
+  }
+
+  #draw_pair (p1, p2, d) {
+    // TODO: implement code to draw a pair of points/vectors
+    // given 2 x, y vectors and a distance
+    ellipse(p1.x, p1.y, 6, 6);
+    ellipse(p2.x, p2.y, 6, 6);
   }
 
   draw() {
     push();
-    // TODO: implement draw code here
+    // Assume center of canvas is at location (0, 0)
+
+    // Always draw p1 <-> p2
+    this.#draw_pair(this.#p1, this.#p2, this.d12);
+
+    // Optionally draw p3 relationships
+    if (this.use_p3) {
+      this.#draw_pair(this.#p1, this.#p3, this.d13);
+      this.#draw_pair(this.#p2, this.#p3, this.d23);
+    }
     pop();
   }
 
+  // getters/setters for p1, p2, & p3
   get p1() {
     return this.#p1;
   }
@@ -72,6 +91,7 @@ class Distance {
   }
 
   set p3(p) {
+    this.use_p3 = true;
     this.#p3.set(p.x, p.y);
 
     this.d13 = this.distance(this.#p1, this.#p3);
@@ -79,4 +99,4 @@ class Distance {
   }
 }
 
-export default Distance;
+export default DistanceMetric;
--- sketch.js
+++ sketch.js
@@ -1,205 +1,137 @@
-const w = 640;
-const h = 480;
+import DistanceMetric from './distance_metric.js';
+
+const canvas_w = 640;
+const canvas_h = 480;
 
 let x = null;
+let y = null;
 let z = null;
-
-let dxy = 0;
-let dxz = 0;
-let dyz = 0;
-
-let selector;
-let dist_calc = 'Euclidean';
+let distance_metric = null;
 
 function setup() {
-  createCanvas(w, h);
-
-  sel = createSelect();
-  sel.position(width * 0.85, height * 0.45);
-  sel.option('Euclidean');
-  sel.option('Manhattan');
-  sel.option('Chebyshev');
-  sel.changed(set_dist_calc);
-}
-
-function draw() {
-  clear();
-  background(0);
-
-  const y = createVector(mouseX, mouseY);
-  const dist_calc = sel.value();
-
-  dxy = 0;
-  dxz = 0;
-  dyz = 0;
-  if (x != null) {
-    dxy += euclidean_distance(x, y, 'x, y', 50);
-    dxy += manhattan_distance(x, y, 'x, y', 70);
-    dxy += chebyshev_distance(x, y, 'x, y', 90);
-
-    strokeWeight(0);
-    fill(255, 0, 0);
-    text('x', x.x - 10, x.y - 10);
-    ellipse(x.x, x.y, 20);
-
-    fill(0, 255, 0);
-    text('y', y.x - 10, y.y - 10);
-    ellipse(y.x, y.y, 20);
-  }
-
-  if (z != null & x != null) {
-    dyz += euclidean_distance(y, z, 'y, z', 120);
-    dyz += manhattan_distance(y, z, 'y, z', 140);
-    dyz += chebyshev_distance(y, z, 'y, z', 160);
-
-    dxz += euclidean_distance(x, z, 'x, z', 190);
-    dxz += manhattan_distance(x, z, 'x, z', 210);
-    dxz += chebyshev_distance(x, z, 'x, z', 230);
-
-    strokeWeight(0);
-    fill(0, 0, 255);
-    text('z', z.x - 10, z.y - 10);
-    ellipse(z.x, z.y, 20);
-
-    fill(255, 0, 0);
-    text('x', x.x - 10, x.y - 10);
-    ellipse(x.x, x.y, 20);
-
-    fill(0, 255, 0);
-    text('y', y.x - 10, y.y - 10);
-    ellipse(y.x, y.y, 20);
-
-    stroke(250);
-    fill(250);
-    strokeWeight(0.5);
-
-    text('Triangle Inequality', 20, 280);
-    text('d(x,y) <= d(x,z) + d(y,z)', 20, 300);
-    text(`${dxy.toFixed(0)} <= ${dxz.toFixed(0)} + ${dyz.toFixed(0)}`, 20, 320);
-    text(`${dxy.toFixed(0)} <= ${(dxz + dyz).toFixed(0)}`, 20, 340);
-  }
+  createCanvas(canvas_w, canvas_h);
 }
 
 function mouseClicked() {
-  x = createVector(mouseX, mouseY);
+  x = createVector(mouseX - width / 2, mouseY - height / 2);
 }
 
 function keyPressed() {
   if (keyCode === ENTER) {
-    z = createVector(mouseX, mouseY);
+    z = createVector(mouseX - width / 2, mouseY - height / 2);
   } else if (keyCode === ESCAPE) {
     z = null;
   }
 }
 
-function set_dist_calc() {
-  dist_calc = sel.value();
-}
+function draw() {
+  background(0);
+  translate(width / 2, height / 2);
 
-function mid_point_text(p1, p2, t) {
-  const mp = createVector((p2.x - p1.x) / 2, (p2.y - p1.y) / 2);
+  // Draw axes with origin in middle
+  stroke(255);
+  strokeWeight(2);
+  line(-width / 2, 0, width / 2, 0);
+  line(0, -height / 2, 0, height / 2);
 
-  push();
-  strokeWeight(0.5);
-  stroke(250);
-  fill(250);
-  translate(p1);
-  text(t, mp.x + 10, mp.y + 20);
-  pop();
-}
+  // Set y location based on mouse
+  y = createVector(mouseX - width / 2, mouseY - height / 2);
 
-function euclidean_distance(p1, p2, name, text_y) {
-  const dist = sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2);
-
-  if (dist_calc == 'Euclidean') {
-    fill(250);
-    stroke(250);
-    strokeWeight(3);
-    line(p1.x, p1.y, p2.x, p2.y);
-    mid_point_text(p1, p2, dist.toFixed(0));
-
-    return_dist = dist;
-  } else {
-    return_dist = 0;
+  // Create dist metric if both x and y exist
+  if (x && y && !distance_metric) {
+    distance_metric = new DistanceMetric(x, y);
   }
 
-  push();
-  strokeWeight(0.5);
-  fill(250);
-  stroke(250);
-  text(`Euclidean d(${name}): ${dist.toFixed(0)}`, 20, text_y);
-  pop();
-
-  return (return_dist);
-}
-
-function manhattan_distance(p1, p2, name, text_y) {
-  const dist_x = abs(p1.x - p2.x);
-  const dist_y = abs(p1.y - p2.y);
-  const dist = dist_x + dist_y;
-
-  if (dist_calc == 'Manhattan') {
-    const l1p1 = createVector(p1.x, p1.y);
-    const l1p2 = createVector(p2.x, p1.y);
-    const l2p1 = createVector(p2.x, p1.y);
-    const l2p2 = createVector(p2.x, p2.y);
-
-    mid_point_text(l1p1, l1p2, dist_x.toFixed(0));
-    mid_point_text(l2p1, l2p2, dist_y.toFixed(0));
-
-    fill(250);
-    stroke(250);
-    strokeWeight(3);
-    line(l1p1.x, l1p1.y, l1p2.x, l1p2.y);
-    line(l2p1.x, l2p1.y, l2p2.x, l2p2.y);
-
-    return_dist = dist;
-  } else {
-    return_dist = 0;
+  // Update y location and draw if dist metric exists
+  if (distance_metric) {
+    distance_metric.p2 = y;
+    distance_metric.draw();
   }
-
-  push();
-  strokeWeight(0.5);
-  fill(250);
-  stroke(250);
-  text(`Manhattan d(${name}): ${dist.toFixed(0)}`, 20, text_y);
-  pop();
-
-  return (return_dist);
 }
 
-function chebyshev_distance(p1, p2, name, text_y) {
-  const dist_x = abs(p1.x - p2.x);
-  const dist_y = abs(p1.y - p2.y);
-  const dist = max([dist_x, dist_y]);
+window.mouseClicked = mouseClicked;
+window.setup = setup;
+window.draw = draw;
 
-  if (dist_calc == 'Chebyshev') {
-    const lp1 = createVector(p1.x, p1.y);
-    let lp2;
-    if (dist == dist_x) {
-      lp2 = createVector(p2.x, p1.y);
-    } else {
-      lp2 = createVector(p1.x, p2.y);
-    }
+// function mid_point_text(p1, p2, t) {
+//   const mp = createVector((p2.x - p1.x) / 2, (p2.y - p1.y) / 2);
 
-    mid_point_text(lp1, lp2, dist.toFixed(0));
+//   push();
+//   strokeWeight(0.5);
+//   stroke(250);
+//   fill(250);
+//   translate(p1);
+//   text(t, mp.x + 10, mp.y + 20);
+//   pop();
+// }
+//
+// function manhattan_distance(p1, p2, name, text_y) {
+//   const dist_x = abs(p1.x - p2.x);
+//   const dist_y = abs(p1.y - p2.y);
+//   const dist = dist_x + dist_y;
 
-    fill(250);
-    stroke(250);
-    strokeWeight(3);
-    line(lp1.x, lp1.y, lp2.x, lp2.y);
+//   if (dist_calc == 'Manhattan') {
+//     const l1p1 = createVector(p1.x, p1.y);
+//     const l1p2 = createVector(p2.x, p1.y);
+//     const l2p1 = createVector(p2.x, p1.y);
+//     const l2p2 = createVector(p2.x, p2.y);
 
-    return_dist = dist;
-  } else {
-    return_dist = 0;
-  }
+//     mid_point_text(l1p1, l1p2, dist_x.toFixed(0));
+//     mid_point_text(l2p1, l2p2, dist_y.toFixed(0));
 
-  push();
-  strokeWeight(0.5);
-  fill(250);
-  stroke(250);
-  text(`Chebyshev d(${name}): ${dist.toFixed(0)}`, 20, text_y);
-  pop();
+//     fill(250);
+//     stroke(250);
+//     strokeWeight(3);
+//     line(l1p1.x, l1p1.y, l1p2.x, l1p2.y);
+//     line(l2p1.x, l2p1.y, l2p2.x, l2p2.y);
 
-  return (return_dist);
-}
+//     return_dist = dist;
+//   } else {
+//     return_dist = 0;
+//   }
+
+//   push();
+//   strokeWeight(0.5);
+//   fill(250);
+//   stroke(250);
+//   text(`Manhattan d(${name}): ${dist.toFixed(0)}`, 20, text_y);
+//   pop();
+
+//   return (return_dist);
+// }
+
+// function chebyshev_distance(p1, p2, name, text_y) {
+//   const dist_x = abs(p1.x - p2.x);
+//   const dist_y = abs(p1.y - p2.y);
+//   const dist = max([dist_x, dist_y]);
+
+//   if (dist_calc == 'Chebyshev') {
+//     const lp1 = createVector(p1.x, p1.y);
+//     let lp2;
+//     if (dist == dist_x) {
+//       lp2 = createVector(p2.x, p1.y);
+//     } else {
+//       lp2 = createVector(p1.x, p2.y);
+//     }
+
+//     mid_point_text(lp1, lp2, dist.toFixed(0));
+
+//     fill(250);
+//     stroke(250);
+//     strokeWeight(3);
+//     line(lp1.x, lp1.y, lp2.x, lp2.y);
+
+//     return_dist = dist;
+//   } else {
+//     return_dist = 0;
+//   }
+
+//   push();
+//   strokeWeight(0.5);
+//   fill(250);
+//   stroke(250);
+//   text(`Chebyshev d(${name}): ${dist.toFixed(0)}`, 20, text_y);
+//   pop();
+
+//   return (return_dist);
+// }
